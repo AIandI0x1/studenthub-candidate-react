4bba46f2c0bbf80d4d5a87c62aca6c7daa4dd468
mehar-bawes <mehar@bawes.net>
2025-07-07 13:33:29 +0530

    Revert "Mehar/tech 816 yii db exception sqlstate42s22 column not found 1054 unknown"

--- src/components/on-board/paged-degree-input.tsx
+++ src/components/on-board/paged-degree-input.tsx
@@ -14,7 +14,6 @@ import { listDegrees } from "@/providers/logged-in/candidate-education.service";
 import { FormInput } from "../ui/form-input";
 import { Card, CardContent } from "../ui/card";
 import { langContent } from "@/utils/common";
-import { z } from 'zod';
 
 interface DegreeInputProps {
     selectedDegree: any | null;
@@ -23,8 +22,6 @@ interface DegreeInputProps {
     required?: boolean;
     form: any;
 }
-
-const degreeNameSchema = z.string().min(1, 'Name is required').regex(/^[a-zA-Z0-9\s\'-.]*$/, 'Only alphanumeric characters, spaces, hyphens, dots, and apostrophes are allowed.');
 
 export default function PagedDegreeInput({ selectedDegree, onSelect, name, form, required = false }: DegreeInputProps) {
 
@@ -39,22 +36,14 @@ export default function PagedDegreeInput({ selectedDegree, onSelect, name, form,
     });
 
     useEffect(() => {
-        const value = form.getValues(name);
-        if (!open) {
+         
+        if (!open || form.getValues(name) == "") {
             return;
         }
-        const result = degreeNameSchema.safeParse(value);
-        if (!result.success) {
-            form.setError(name, { type: 'manual', message: result.error?.errors[0]?.message });
-            return;
-        } else {
-            form.clearErrors(name);
-        }
-        if (value !== '') {
-            loadPage(1);
-        }
-    }, [form.watch(name)]);
 
+        loadPage(1)
+
+    }, [form.watch(name)])
  
     const handleSelect = (degree: any) => {
         setOpen(false)
